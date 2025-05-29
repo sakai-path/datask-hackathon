1b2ceeff6bd2668378c520749da69c89bf07335c
Sakai <sakai0920@gmail.com>
2025-05-29 15:25:41 +0900

    Update app.py

--- datask_app/app.py
+++ datask_app/app.py
@@ -55,9 +55,9 @@ if st.button("利用回数グラフを表示"):
     df = get_seat_usage_counts(engine)
     draw_usage_bar_chart(df)
 
+"""
 from testdata.seatlog_dummy import create_test_logs
 
-"""
 with st.expander("※初期テストデータ登録", expanded=False):
     if st.button("SeatLog ダミーデータを登録"):
         create_test_logs()
